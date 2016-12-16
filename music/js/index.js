6d4220d4bdd57248b8954a40f09332dbcc757b60
941029minmin <1787290283@qq.com>
2016-12-16 22:42:44 +0800

    two

--- music/js/index.js
+++ music/js/index.js
@@ -9,6 +9,24 @@ function resize(originSize,type) {
     }
     document.getElementsByTagName("html")[0].style.fontSize = scale+"px";
 }
+
 window.onload = function() {
     resize(750);
+     var tabsSwiper = new Swiper('#tabs-container',{
+    speed:500,
+    onSlideChangeStart: function(){
+      $(".tabs .active").removeClass('active')
+      $(".tabs a").eq(tabsSwiper.activeIndex).addClass('active')  
+    }
+  })
+  $(".tabs a").on('touchstart mousedown',function(e){
+    e.preventDefault()
+    $(".tabs .active").removeClass('active')
+    $(this).addClass('active')
+    tabsSwiper.slideTo( $(this).index() )
+  })
+  $(".tabs a").click(function(e){
+    e.preventDefault()
+  })
+  
 }
